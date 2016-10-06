58d8f056d35c61d89f637e03d450aab370a72055
leijurv <leijurv@gmail.com>
2016-10-06 14:59:40 -0700

    updated

--- src/syncer/GUI.java
+++ src/syncer/GUI.java
@@ -18,7 +18,6 @@ import javax.swing.JFrame;
 import javax.swing.WindowConstants;
 import static syncer.Syncer.BYTES_PER_SEC;
 import static syncer.Syncer.SIZE;
-import static syncer.Syncer.cache;
 
 /**
  *
@@ -173,73 +172,10 @@ public class GUI {
                 //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
             }
         });
-        long startAbove = 0;
-        while (true) {
-            synchronized (cache.lock) {
-                int size = cache.getSize();
-                String temp = "";
-                temp += ("Number of seconds output thread spent waiting for data: " + cache.waitMS / 1000F);
-                temp += "\n";
-                cache.waitMS = 0;
-                temp += ("Cache size: " + size);
-                temp += "\n";
-                float seconds = (size * SIZE) / ((float) BYTES_PER_SEC);
 
-                temp += ("Number of seconds currently in cache: " + seconds);
-                temp += "\n";
-                boolean aboveLong = false;
-                if (size > 1) {
-                    if (startAbove == 0) {
-                        startAbove = System.currentTimeMillis();
-                    }
-
-                    if (startAbove + 2000 < System.currentTimeMillis()) {
-                        aboveLong = true;
-                    }
-                } else {
-                    startAbove = 0;
-                }
-                temp += ("ms since zero cache: " + (size > 1 ? (System.currentTimeMillis() - startAbove) : 0));
-                temp += "\n";
-                temp += ("Cutting: " + aboveLong);
-                temp += "\n";
-                temp += "Offset between server multiplex time and receive: " + offset2;
-                temp += "\n";
-                temp += "Offset between server multiplex time and fully received: " + offset3;
-                temp += "\n";
-                temp += "Offset between server multiplex time and began write to sox subprocess (this is the important one): " + offset;
-                info = temp;
-                M.repaint();
-                if (aboveLong) {
-                    int target = (int) Math.floor(Math.max(0, size * 0.98F));
-                    while (Syncer.cache.getSize() > target) {
-                        Syncer.cache.beginning = Syncer.cache.beginning.next;
-                    }
-                    if (seconds > 2) {
-                        //beginning = null;
-                    }
-                    if (System.currentTimeMillis() - startAbove > 20000) {
-                        System.out.println("Exiting because the cache has been nonzero for more than 20 seconds");
-                        Thread.sleep(1000);
-                        System.exit(1);
-                    }
-                }
-                /*if (verbose) {
-                    System.out.println(temp);
-                }*/
-
-            }
-            Thread.sleep(50);
-        }
     }
-    static long offset;
-    static long offset2;
-    static long offset3;
 
     public static void onData(Chunk chunk) {
-        offset = System.currentTimeMillis() - chunk.multiplexed / 1000000;
-        offset2 = chunk.beganToReceive - chunk.multiplexed / 1000000;
-        offset3 = chunk.received - chunk.multiplexed / 1000000;
 
         byte[] toWrite = chunk.contents;
         long start = System.currentTimeMillis();
--- src/syncer/Syncer.java
+++ src/syncer/Syncer.java
@@ -10,8 +10,10 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.Socket;
+import java.util.Arrays;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -21,6 +23,7 @@ public class Syncer {
 
     static final int SIZE = 4096;
     static final int BYTES_PER_SEC = 44100 * 4;
+    static boolean gui = true;
 
     /**
      * @param args the command line arguments
@@ -44,8 +47,13 @@ public class Syncer {
             return;
         }
 
+        boolean verbose = Arrays.asList(args).contains("verbose");
+        gui = !Arrays.asList(args).contains("nogui");
+        if (gui) {
+
+            gui = 0 == JOptionPane.showConfirmDialog(null, "Would you a GUI or nah?", "It'll work either way", JOptionPane.YES_NO_OPTION);
+        }
         InputStream in = new Socket("207.47.5.28", 5021).getInputStream();
-        boolean verbose = args.length > 0 && args[0].equals("verbose");
         new CacheSource(new DataInputStream(in), cache).start();
         cache.sleepUntilSize(20);
         Process sox = new ProcessBuilder("/usr/local/bin/sox -t raw -r 44100 -b 16 -c 2 --buffer 4096 -e signed-integer - -tcoreaudio --buffer 4096".split(" ")).start();
@@ -74,10 +82,19 @@ public class Syncer {
             public void run() {
                 try {
                     while (true) {
-                        Chunk toWrite = cache.getChunk();
-                        sox.getOutputStream().write(toWrite.contents);
-                        GUI.onData(toWrite);
+                        Chunk chunk = cache.getChunk();
+                        sox.getOutputStream().write(chunk.contents);
+                        if (gui) {
+                            GUI.onData(chunk);
+                        }
+                        long lad = System.currentTimeMillis();
+                        offset = lad - chunk.multiplexed / 1000000;
+                        offset2 = chunk.beganToReceive - chunk.multiplexed / 1000000;
+                        offset3 = chunk.received - chunk.multiplexed / 1000000;
                         sox.getOutputStream().flush();
+                        long wew = System.currentTimeMillis();
+                        offset4 = wew - lad;
+                        offset5 = wew - chunk.multiplexed / 1000000;
                         //System.out.println("wew");
                     }
                 } catch (IOException ex) {
@@ -91,10 +108,79 @@ public class Syncer {
                 }
             }
         }.start();
+        if (gui) {
+            GUI.begin();
+        }
+        long startAbove = 0;
+        while (true) {
+            synchronized (cache.lock) {
+                int size = cache.getSize();
+                String temp = "";
+                temp += ("Number of seconds output thread spent waiting for data: " + cache.waitMS / 1000F);
+                temp += "\n";
+                cache.waitMS = 0;
+                temp += ("Cache size: " + size);
+                temp += "\n";
+                float seconds = (size * SIZE) / ((float) BYTES_PER_SEC);
 
-        GUI.begin();
+                temp += ("Number of seconds currently in cache: " + seconds);
+                temp += "\n";
+                boolean aboveLong = false;
+                if (size > 1) {
+                    if (startAbove == 0) {
+                        startAbove = System.currentTimeMillis();
+                    }
+
+                    if (startAbove + 2000 < System.currentTimeMillis()) {
+                        aboveLong = true;
+                    }
+                } else {
+                    startAbove = 0;
+                }
+                temp += ("ms since zero cache: " + (size > 1 ? (System.currentTimeMillis() - startAbove) : 0));
+                temp += "\n";
+                temp += ("Cutting: " + aboveLong);
+                temp += "\n";
+                temp += "Offset between server multiplex time and receive: " + offset2;
+                temp += "\n";
+                temp += "Offset between server multiplex time and fully received: " + offset3;
+                temp += "\n";
+                temp += "Subprocess flush time: " + offset4;
+                temp += "\n";
+                temp += "Offset between server multiplex time and end write to sox subprocess: " + offset5;
+                temp += "\n";
+                temp += "Offset between server multiplex time and began write to sox subprocess (this is the important one): " + offset;
+                if (gui && GUI.M != null) {
+                    GUI.info = temp;
+                    GUI.M.repaint();
+                }
+                if (aboveLong) {
+                    int target = (int) Math.floor(Math.max(0, size * 0.98F));
+                    while (Syncer.cache.getSize() > target) {
+                        Syncer.cache.beginning = Syncer.cache.beginning.next;
+                    }
+                    if (seconds > 2) {
+                        //beginning = null;
+                    }
+                    if (System.currentTimeMillis() - startAbove > 20000) {
+                        System.out.println("Exiting because the cache has been nonzero for more than 20 seconds");
+                        Thread.sleep(1000);
+                        System.exit(1);
+                    }
+                }
+                /*if (verbose) {
+                    System.out.println(temp);
+                }*/
+
+            }
+            Thread.sleep(50);
+        }
     }
-
+    static long offset;
+    static long offset2;
+    static long offset3;
+    static long offset4;
+    static long offset5;
     static AudioCache cache = new AudioCache();
 
 }
